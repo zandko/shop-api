b3d76a71c10e9389b8993ef73f59c3610d717c3e
高赞 <gaozan@anchnet.com>
2019-06-27 19:36:47 +0800

    产品列表

--- app/controller/admin/product.js
+++ app/controller/admin/product.js
@@ -4,7 +4,18 @@ const Controller = require('egg').Controller;
 
 class ProductController extends Controller {
   async index () {
+    const { ctx } = this;
 
+    const page = Number(ctx.request.query.page) || 1;
+    const pageSize = Number(ctx.request.query.pageSize) || 3;
+    const total = await ctx.model.Product.find({}).count();
+    const data = await ctx.model.Product.find({}).skip((page - 1) * pageSize).limit(pageSize);
+
+    ctx.helper.success(ctx, {
+      data,
+      page,
+      total
+    })
   }
 
   async store () {
@@ -14,7 +25,7 @@ class ProductController extends Controller {
     await productResult.save();
     const productImageResult = ctx.request.body.image_url;
     const productAttributeResult = ctx.request.body.attr_value;
-    if(productResult._id) {
+    if (productResult._id) {
       if (productImageResult.length > 0) {
         for (let index = 0; index < productImageResult.length; index++) {
           await ctx.model.ProductImage.create({ "product_id": productResult._id, "url": productImageResult[index] });
@@ -22,25 +33,26 @@ class ProductController extends Controller {
       }
       if (productAttributeResult.length > 0) {
         for (let index = 0; index < productAttributeResult.length; index++) {
-          const productAttribute = await ctx.model.ProductTypeAttribute.find({ 
-            "_id": productAttributeResult[index]._id, 
-          });   
-          await ctx.model.ProductAttribute.create({ 
-            "product_id": productResult._id,
-            "product_type_id": ctx.request.body.product_type_id, 
-            "product_category_id": ctx.request.body.product_category_id,
-            "title": productAttribute[0].title,                   
-            "type": productAttribute[0].attr_type,                    
-            "value": productAttributeResult[index].attr_value 
+          const productAttribute = await ctx.model.ProductTypeAttribute.find({
+            "_id": productAttributeResult[index]._id,
           });
-          
+          await ctx.model.ProductAttribute.create({
+            "product_id": productResult._id,
+            "product_type_id": ctx.request.body.product_type_id,
+            "product_category_id": ctx.request.body.product_category_id,
+            "title": productAttribute[0].title,
+            "type": productAttribute[0].attr_type,
+            "value": productAttributeResult[index].attr_value
+          });
         }
       }
+
+      ctx.helper.created(ctx, productResult);
     }
   }
 
   async destroy () {
-    
+
   }
 
   async update () {
